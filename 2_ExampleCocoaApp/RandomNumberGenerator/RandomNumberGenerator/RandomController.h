d1c39acd4627894e520894ae2eeab8d673fb8dd7
Nikita Govind <nikita.govind30@gmail.com>
2018-02-04 11:32:45 -0800

    Some changes

--- 2_ExampleCocoaApp/RandomNumberGenerator/RandomNumberGenerator/RandomController.h
+++ 2_ExampleCocoaApp/RandomNumberGenerator/RandomNumberGenerator/RandomController.h
@@ -20,7 +20,19 @@
 // IBOutlet is a macro that evaluates to nothing. IBOutlet is a hint to Interface Builder when it reads the declaration of a class from a .h file
 @property (weak) IBOutlet NSTextField *resultTextField;
 
-// IBAction is the same as void. It also acts as a hint to Interface Builder.
+/*
+ * IBAction is the same as void. It just acts as a hint to Interface Builder that this is an actiona ssociated with a Cocoa object.
+ * They always take the sender as an arg bec this tells the sender which control sent the message
+ * Eg. where this info is important? If it is a Checkbox and the box value toggled say from Yes to NO
+ * the checkbox button will be pointed to the action mehtod
+ - (IBAction)toggleCheckbox:(id)sender
+ {
+    // Now to read the value of the checkbox, we need the sender object
+    BOOL isOn = [sender state];
+    ...
+ }
+ 
+ */
 - (IBAction) randomlyGenerateNumber: (id)sender;
 - (IBAction) randomlyGenerateNumberBasedOffTime    : (id)sender;
 
